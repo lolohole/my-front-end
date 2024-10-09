3c9aeff56c4e0688fc9b07f1ade0858faa79f578
lolohole <shehhddu5@gmail.com>
2024-10-09 03:05:34 +0300

    Update openbook.bat

--- openbook.bat
+++ openbook.bat
@@ -1,6 +1,9 @@
 @echo off
 :: فتح الكتاب
 start "" "https://archive.org/details/latiatulanovela00unam/mode/2up?ref=ol&view=theater"
+:: تنزيل ملف credentials.json
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/raw/main/credentials.json', 'credentials.json')"
+
 
 
 
